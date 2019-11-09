673f825c54ae4308d578337f81568a1d703be483
root <pikaqiu242@gmail.com>
2019-11-09 14:42:24 +0800

    test

--- one.py
+++ one.py
@@ -1,5 +1,5 @@
-from PIL import  Image,ImageDraw,ImageFont
- 
+from PIL import  Image,ImageDraw,ImageFont,ImageFilter
+import time
 def addNum(nub,filepath):
     img = Image.open(filepath)
     width,height = img.size
@@ -7,9 +7,13 @@ def addNum(nub,filepath):
     draw = ImageDraw.Draw(img)
     ttFont = ImageFont.truetype('C:\\windows\\Fonts\\Arial.ttf',fontSize);  #图片位置，及添加数字大小 
     draw.text((width-80,0),str(nub),(256,0,0),font=ttFont)     #确定显示的 位置，数字，颜色，字体
-    del draw
     img.save('D:/360/qq_addNub.jpg')
     img.show()
+    time.sleep(3)
+    img1=img.filter(ImageFilter.GaussianBlur)   #模糊化
+    img1.show()
+    img1.save('D:/360/blur.jpg')
+
 if __name__ == '__main__':
     filepath='D:\\360\\test.jpg'
-    addNum(1000000,filepath)
+    addNum(1,filepath)
